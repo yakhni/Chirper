3c2ce0da90f1794d90d6bb1708e856437aff803f
Farhan Ul Haq <farhan@openvpn.net>
2020-12-28 21:56:39 -0800

    NewTweet Action creator

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,6 +1,10 @@
 import { saveLikeToggle } from '../utils/api'
+import { saveTweet } from '../utils/api'
+import { showLoading, hideLoading } from 'react-redux-loading'
+
 export const TOGGLE_TWEET = 'TOGGLE_TWEET'
 export const RECEIVE_TWEETS = 'RECEIVE_TWEETS'
+export const ADD_TWEET = 'ADD_TWEET'
 
 export function receiveTweets (tweets) {
   return {
@@ -15,6 +19,28 @@ function toggleTweet({ id, authedUser, hasLiked }) {
     id,
     authedUser,
     hasLiked
+  }
+}
+
+function newTweet(tweet) {
+  return {
+    type: ADD_TWEET,
+    tweet
+  }
+}
+
+export function handleNewTweet(text, replyingTo) {
+  return (dispatch, getState) => {
+    const { authedUser} = getState()
+
+    dispatch(showLoading())
+    return saveTweet({
+      text,
+      author: authedUser,
+      replyingTo
+    })
+      .then((tweet) => dispatch(newTweet(tweet)))
+      .then(() => dispatch(hideLoading()))
   }
 }
 
